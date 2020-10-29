b7216850e488dc832acf0f505bd7ccd2321e9391
nosam1998 <nosam1998@gmail.com>
2020-10-29 05:22:20 -0700

    Added post route to receive a link to an image and respond with a json object.

--- routes/api.js
+++ routes/api.js
@@ -1,9 +1,26 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const { createWorker } = require('tesseract.js');
 
 /* GET home page. */
-router.post('/', function(req, res, next) {
-  res.json({});
+router.post('/', function (req, res, next) {
+  let img_path = req.body.imagePath;
+
+  const worker = createWorker({
+    logger: m => console.log(m), // Add logger here
+  });
+
+  (async () => {
+    await worker.load();
+    await worker.loadLanguage('eng');
+    await worker.initialize('eng');
+    const { data: { text } } = await worker.recognize(img_path);
+    console.log(text);
+    res.json({text: text});
+    await worker.terminate();
+  })();
+
+
 });
 
 module.exports = router;
